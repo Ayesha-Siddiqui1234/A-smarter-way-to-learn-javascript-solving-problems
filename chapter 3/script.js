9c66cb42e6fe0cc8a9365eae94d4654879495891
Ayesha Siddiqui <155626270+Ayesha-Siddiqui1234@users.noreply.github.com>
2024-08-13 00:59:08 +0500

    Add files via upload

---
+++ chapter 3/script.js
@@ -0,0 +1,37 @@
+                        //QUESTION :1
+// var age=15;
+// alert("I am " + age +" years old");
+
+  
+
+
+
+                           //QUESTION:2
+// var numOfVisits = 14;
+// alert("You have visited this site " +numOfVisits+ " times");
+
+
+
+
+
+
+                            //QUESTION:3
+
+// var birthYear=2005
+// document.write("My birth year is " + birthyear + "<br>");
+// document.write("Data type of my declared variable is number <br>");
+
+
+
+
+
+
+                               //QUESTION:4
+
+// var name="ayesha";
+// var tittle=" T-shirts";
+// var quantity=5;
+// document.write("<b>" + name + "</b>"   + " "+ "ordered" +" " +"<b>" + quantity +" " +tittle +"</b>"  + "(s) on XYZ Clothing store" + "<br>");
+
+
+             
---
+++ chapter 4/script1.js
@@ -0,0 +1,21 @@
+                            //QUESTION:1
+// I have declared 3 variable in 1 statement
+// var num=32, string_num="ayesha" , floatin_num=3.3;  
+
+
+
+                              //QUESTION:2
+// // 5 legal variable name
+// var _num=23, alpha="ayesha", apha123=89 , ayesha_riaz="name" , $cope='g';
+
+// // 5 illegal variable name
+// var 4rope=8 , apha#=3 , 76alpha=7 , name is="name" , &oop=8 ; 
+
+
+
+                               //QUESTION:3 
+// document.write(" <h3> Rules for naming JS variables </h3> ");
+// document.write("Variable names can only contain , numbers, $ and _. For example :$my_1stVariable <br>");
+// document.write("Variable must begin with a letter, $ or _ . For example:$name,_name or name <br>");
+// document.write("Varaible names are case sensitive <br>");
+// document.write("Varaible name should not be JS keywords <br>");
