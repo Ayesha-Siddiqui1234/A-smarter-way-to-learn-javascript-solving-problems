142b3ce3c00fc75ebe4fa3bbe54787ab6735561d
Ayesha Siddiqui <155626270+Ayesha-Siddiqui1234@users.noreply.github.com>
2024-08-18 01:53:45 +0500

    Add files via upload

---
+++ chapter 5/script5.js
@@ -0,0 +1,170 @@
+// QUESTION:1
+// var num1=3;
+// var num2=5;
+// var add=num1+num2;
+// document.write(`Sum of ${num1} and ${num2} is ${add} <br> `);
+
+
+
+
+//QUESTION:2
+// var num1=3;
+// var num2=5;
+// var sub=num1-num2;
+// var multi=num1*num2;
+// var div=num1/num2;
+// document.write(`Subtraction of ${num1} and ${num2} is ${sub} <br> `);
+// document.write(`Multiplication of ${num1} and ${num2} is ${multi} <br> `);
+// document.write(`Division of ${num1} and ${num2} is ${div} <br> `);
+
+
+
+
+
+//QUESTION:3
+// var num;
+// document.write(`Value after variable declaration is ${num} <br> `);
+// num=5;
+// document.write(`Intial value: ${num} <br>`);
+// num++;
+// document.write(`Value after increment is: ${num} <br>`);
+// num+=7;
+// document.write(`Value after addition is: ${num}<br>`);
+// num--;
+// document.write(`Value after decrement is: ${num}<br>`);
+// var rem=num%3;
+// document.write(`The remainder is: ${rem}`);
+
+
+
+
+
+//QUESTION:4
+// var costEach=600;
+// var costFive=600*5;
+// document.write(`Total cost of buy 5 tickets to a movie is ${costFive}PKR <br> `);
+
+
+
+
+//QUESTION:5
+// document.write("Table of 4 <br> ");
+// var num=4;
+// document.write(`${num} x 1 = ${num*1}  <br>`);
+// document.write(`${num} x 2 = ${num*2}  <br>`);
+// document.write(`${num} x 3 = ${num*3}  <br>`);
+// document.write(`${num} x 4 = ${num*4}  <br>`);
+// document.write(`${num} x 5 = ${num*5}  <br>`);
+// document.write(`${num} x 6 = ${num*6}  <br>`);
+// document.write(`${num} x 7 = ${num*7}  <br>`);
+// document.write(`${num} x 8 = ${num*8}  <br>`);
+// document.write(`${num} x 9 = ${num*9}  <br>`);
+// document.write(`${num} x 10 = ${num*10}  <br>`);
+
+
+
+
+
+//QUESTION:6
+// var celsiusTemp=25;
+// var fahrenheit=(celsiusTemp*9/5)+32;
+// document.write(`${celsiusTemp}°C is ${fahrenheit}°F <br>`);   //shortcut for degree would be alt 0176 
+
+// var fahrenheitTemp=70;
+// var celsius=(fahrenheitTemp-32)*5/9;
+// document.write(`${fahrenheitTemp}°C is ${celsius}°C  <br>`);
+
+
+
+
+
+// //QUESTION:7
+// document.write(`<h1>Shopping Cart</h1>`);
+// var price_of_item_1= 650;
+// var price_of_item_2=100;
+// var ordered_quantity_of_item_1=3;
+// var ordered_quantity_of_item_2=7;
+// var shipping_charges=100;
+// var Total_cost= (price_of_item_1*3) + (price_of_item_2*7) +  shipping_charges ;
+// document.write(`Price of item 1 is ${price_of_item_1} <br>`);
+// document.write(`Quantity of item 1 is ${ordered_quantity_of_item_1} <br>`);
+// document.write(`Price of item 2 is ${price_of_item_2} <br>`);
+// document.write(`Quantity of item 2 is ${ordered_quantity_of_item_2} <br>`);
+// document.write(`Shiping Charges ${shipping_charges} <br><br>`);
+// document.write(`Total cost of your order is  ${Total_cost} <br>`);
+
+
+
+
+//QUESTION:8
+// document.write(`<h1>Mark Sheet</h1>`);
+// var totalMarks=980;
+// var obtainedMarks=804;
+// var percentage=(obtainedMarks/totalMarks)*100;
+// document.write(`Total marks: ${totalMarks}<br>`);
+// document.write(`Marks obtained: ${obtainedMarks} <br>`);
+// document.write(`Percentage: ${percentage} <br>`);
+
+
+
+
+
+//QUESTION:9
+//  document.write(`<h1>Currency in PKR</h1>`);
+// var usDollars=10;
+// var saudiRiyals=25;
+// var us_Dollars_To_Pak_Currency=usDollars*104.80;
+// var saudi_Riyals_To_Pak_Currency=saudiRiyals*28;
+// var total=us_Dollars_To_Pak_Currency+saudi_Riyals_To_Pak_Currency;
+// document.write(`Total Currency in PKR: ${total}<br>`);
+
+
+
+
+
+//QUESTION:10
+// var num=3;
+// var exp=(num+5)*10/2;
+// document.write(`the result of the expression is ${exp}<br>`);
+
+
+
+
+//QUESTION:11
+// var currentYear=2016;
+// var birthYear=1992;
+// var ageOne=currentYear-birthYear;
+// var ageTwo=ageOne-1;
+// document.write(`Current Year: ${currentYear}<br>`);
+// document.write(`Birth Year: ${birthYear}<br>`);
+// document.write(`Your age is: ${ageOne}<br>`);
+// document.write(`They are either ${ageOne} or ${ageTwo} years old <br>`);
+
+
+
+
+//QUESTION:12
+//  document.write(`<h1>The Geometrizer</h1>`);
+// var radius=20;
+// var circumference=2*3.142*radius;
+// var area=3.142*radius*radius;
+// document.write(`Radius of a circle: ${radius}<br>`);
+// document.write(`The circumference is: ${circumference}<br>`);
+// document.write(`The area is: ${area}<br>`);
+
+
+
+//QUESTION:13
+//  document.write(`<h1>The Lifetime Supply Calculator</h1>`);
+// var favSnake="chocolate chip";
+// var currentAge=15;
+// var maxAge=65;
+// var amount_Of_Snake_Per_Day=3;
+// document.write(`Favourite Snake: ${favSnake}<br>`);
+// document.write(`Current age: ${currentAge}<br>`);
+// document.write(`Estimated Maximum Age: ${maxAge}<br>`);
+// document.write(`Amount of snake per day: ${amount_Of_Snake_Per_Day}<br>`);
+// var numOfSnake=(maxAge-currentAge)*365; //in example e result isnot printed so i just gave  here formula for 
+// var eatRestOfLife=numOfSnake*amount_Of_Snake_Per_Day;  //solving (e) part 
+// var num_Of_SNake_By_SUbtracting_Current_Age=(maxAge-currentAge)*amount_Of_Snake_Per_Day;
+// document.write(`Your will need ${num_Of_SNake_By_SUbtracting_Current_Age} chocolate chip to last you until the rip old age of ${maxAge}<br>`);
