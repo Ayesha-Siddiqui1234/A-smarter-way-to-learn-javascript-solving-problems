d6ab9a71ee57a35898b598454d24e16759befbbe
Ayesha Siddiqui <155626270+Ayesha-Siddiqui1234@users.noreply.github.com>
2024-10-05 20:33:27 +0500

    Add files via upload

---
+++ chapter 14-16/script.js
@@ -0,0 +1,123 @@
+//QUESTION 1
+// let studentNames = [];
+
+//QUESTION 2
+// let studentNames = new Array();
+
+//QUESTION 3
+// let stringsArray = ["Ali", "koko", "toto"];
+
+//QUESTION 4
+//let numbersArray = [10, 20, 30, 40, 50];
+
+//QUESTION 5
+//let booleanArray = [true, false, true, false];
+
+//QUESTION 6
+//let mixedArray = ["ali", 25, true, "Engineer", 99.9];
+
+//QUESTION 7
+//in html file
+
+//QUESTION 8
+
+// let studentNames = ["Micheal", "John", "Tony"];
+// let studentScores = [320, 230, 480];
+// let totalMarks = 500;
+
+// for (let i = 0; i < studentNames.length; i++) {
+//     let percentage = (studentScores[i] / totalMarks) * 100;
+//     console.log(`Student: ${studentNames[i]}, Score: ${studentScores[i]}, Percentage: ${percentage.toFixed(2)}%`);
+// }
+
+
+//QUESTION 9
+// let colors = ["Red", "Green", "Blue", "Yellow", "Purple"];
+// console.log("Initial colors: " + colors);
+
+// let colorToAddAtBeginning = prompt("Enter a color to add to the beginning of the array:");
+// colors.unshift(colorToAddAtBeginning);
+// console.log("After adding a color at the beginning: " + colors);
+
+// let colorToAddAtEnd = prompt("Enter a color to add to the end of the array:");
+// colors.push(colorToAddAtEnd);
+// console.log("After adding a color at the end: " + colors);
+
+// colors.unshift("Orange", "Pink");
+// console.log("After adding two more colors at the beginning: " + colors);
+
+// colors.shift();
+// console.log("After deleting the first color: " + colors);
+
+// colors.pop();
+// console.log("After deleting the last color: " + colors);
+
+// let indexToAdd = parseInt(prompt("At which index do you want to add a color?"));
+// let colorToAdd = prompt("Enter the color name to add:");
+// colors.splice(indexToAdd, 0, colorToAdd);
+// console.log("After adding a color at index " + indexToAdd + ": " + colors);
+
+// let indexToDelete = parseInt(prompt("At which index do you want to delete color(s)?"));
+// let numberOfColorsToDelete = parseInt(prompt("How many colors do you want to delete?"));
+// colors.splice(indexToDelete, numberOfColorsToDelete);
+// console.log("After deleting " + numberOfColorsToDelete + " color(s) from index " + indexToDelete + ": " + colors);
+
+
+//QUESTION 10
+// let studentScores = [230, 320, 480, 120];
+// console.log("Original scores: " + studentScores);
+
+// studentScores.sort((a, b) => a - b);
+// console.log("Sorted scores in ascending order: " + studentScores);
+
+//QUESTION 11
+// let cities = ["Karachi", "Lahore", "Islamabad", "Quetta", "Peshawar"];
+// let selectedCities = [];
+// selectedCities.push(cities[2]); 
+// selectedCities.push(cities[3]);
+
+// console.log("Cities: " + cities);
+// console.log("Selected Cities: " + selectedCities);
+
+
+//QUESTION 12
+// var arr = ["This ", "is ", "my ", "cat"];
+// var resultString = arr.join(""); 
+// console.log(resultString);
+
+//QUESTION 13
+// let devicesQueue = [];
+
+// devicesQueue.push("keyboard");
+// devicesQueue.push("mouse");
+// devicesQueue.push("printer");
+// devicesQueue.push("monitor");
+
+// console.log("Devices in Queue:", devicesQueue);
+
+// console.log("Dequeued:", devicesQueue.shift());
+// console.log("Dequeued:", devicesQueue.shift());
+// console.log("Dequeued:", devicesQueue.shift());
+// console.log("Dequeued:", devicesQueue.shift());
+
+// console.log("Dequeued:", devicesQueue.shift());
+
+//QUESTION 14
+// let devicesStack = [];
+
+// devicesStack.push("keyboard");
+// devicesStack.push("mouse");
+// devicesStack.push("printer");
+// devicesStack.push("monitor");
+
+// console.log("Devices in Stack:", devicesStack);
+// console.log("Popped:", devicesStack.pop()); 
+// console.log("Popped:", devicesStack.pop()); 
+// console.log("Popped:", devicesStack.pop());
+// console.log("Popped:", devicesStack.pop()); 
+
+// console.log("Popped:", devicesStack.pop()); 
+
+//QUESTION 15
+//check html file
+
