589a7c79a803f8bba4dcfb32c1deab978f79b37e
Ayesha Siddiqui <155626270+Ayesha-Siddiqui1234@users.noreply.github.com>
2024-10-05 19:25:45 +0500

    Add files via upload

---
+++ chapter12-13/script.js
@@ -0,0 +1,99 @@
+//question 1
+// let inputvalues=prompt(`Enter any value which you have to check`);
+// let inputvalue=inputvalues.charCodeAt(0);
+// if(inputvalue>=65 && inputvalue<=90){
+//     document.write(`The input is upper case`);
+// }
+// else if(inputvalue>=97 && inputvalue<=122){
+//     document.write(`The input is lower case`);
+// }
+// else{
+//     document.write(`the input is a number`);
+// }
+
+//QUESTION:2
+//  let inputvalues=prompt(`Enter any value which you have to check`);
+//  let inputvalues1=prompt(`Enter another value which you have to check`);
+//  if(inputvalues==inputvalues1){
+//     document.write(`Both values are equal`);
+//  }
+//  else if(inputvalues>inputvalues1){
+//     document.write(`${inputvalues} is greater than ${inputvalues1}`);
+//  }
+//  else if(inputvalues<inputvalues1){
+//     document.write(`${inputvalues} is slower than ${inputvalues1}`);
+//  }
+// else{
+//     document.write(`Try again`);
+// }
+
+
+//QUESTION 3    
+// let inputvalue=prompt(`Enter any value which you have to check`);
+
+// if(inputvalue>1){
+//     document.write(`The number is positive`);
+// }
+// else if(inputvalue<1 && inputvalue!=0){
+//     document.write(`The input is negative`);
+// }
+// else{
+//     document.write(`the input is 0`);
+// }
+
+//QUESTION 4
+// let inputvalues=prompt(`Enter the value`);
+// let inputvalue=inputvalues.charCodeAt(0);
+// if(inputvalue==97 || inputvalue==101 || inputvalue==105 || inputvalue==111 || inputvalue==117){
+//     document.write(`The entered charcter is vowel `);
+// }
+// else{
+//     document.write(`The input value is not a vowel `);
+// }
+
+//QUESTION 5
+// let js="ayesha siddiqui";
+// let inputval=prompt(`Enter the password`);
+// if(inputval.trim( ) === ""){
+//     document.write(`String is empty ...plz enter your password`);
+// }
+// else if(js==inputval){
+//     document.write(`Correct !! The password you have entered matches the orignal password `);
+// }
+   
+// else
+// {
+//     document.write(`you have entered wrong password `);
+// }
+
+//QUESTION 6
+// var greeting;
+// var hour=13;
+// if(hour<18){
+//     greeting="Good day";
+//     document.write(`${greeting}`);
+// }
+// else{
+//     greeting="Good evening";
+//     document.write(`${greeting}`);
+// }
+
+
+//QUESTION 7
+// let time=prompt(`Enter the time in 24 hrs formate`);
+// if(time >= 0 && time<1200){
+// document.write(`Good Morning`);
+// }
+// else if(time>= 1200 && time<1700){
+//     document.write(`Good afternoon`);
+// }
+
+// else if(time>=1700 && time<2100){
+//     document.write(`Good Evening`);
+// }
+// else if(time>=2100 && time<2359){
+//     document.write(`Good Night!!`);
+// }
+// else{
+//     document.write(`Invalid input`);
+// }
